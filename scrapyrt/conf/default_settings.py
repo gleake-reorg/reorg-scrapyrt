a0a2e7afabf5b62fac8c2f752bcb73e6d5d43630
Gareth Leake <159931779+gleake-reorg@users.noreply.github.com>
2024-05-31 16:45:35 -0400

    Update default_settings.py

--- scrapyrt/conf/default_settings.py
+++ scrapyrt/conf/default_settings.py
@@ -3,9 +3,6 @@ from dotenv import load_dotenv
 # because this code is run as a dependency to Pacer-Scraper, we need to load the .env from the main project level
 load_dotenv("/scrapyrt/src/.env")
 
-for k, v in os.environ.items():
-    print(f'{k}: {v}')
-
 # -*- coding: utf-8 -*-
 """Default scrapyrt settings."""
 
